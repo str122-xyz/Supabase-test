271aeff490a2cb04398c58e65996997c7a8e2c7c
Satriahrl_ <satriaherlambang22@gmail.com>
2025-12-18 23:39:08 +0700

    feat: setup class and initial scaffolding

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,1 +1,25 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(children: []),
+      ),
+    );
+  }
+}
