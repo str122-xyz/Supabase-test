3e6c330cdafa777eb779b3457983724a6dcef5ae
Satriahrl_ <satriaherlambang22@gmail.com>
2025-12-19 00:15:14 +0700

    feat: initiaze project url and API key

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,9 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'MASUKKAN_PROJECT_URL_DISINI', //Todo project url
-    anonKey: 'MASUKKAN_ANON_PUBLIC_KEY_DISINI', //Todo public key
+    url: 'https://bhvnczdawzgyljukdvvi.supabase.co', //Project URL
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJodm5jemRhd3pneWxqdWtkdnZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNzA1NzIsImV4cCI6MjA4MTY0NjU3Mn0.-igzqfDqtE8glX99I41IuLs7JoHOZ-yz0tpP44Etu84', //API Key
   );
 
   runApp(const MyApp());
