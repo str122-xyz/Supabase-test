27d23745caaadf3f8b6d39a7ec3287508313bbde
Satriahrl_ <satriaherlambang22@gmail.com>
2025-12-19 00:02:02 +0700

    feat: logic

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,6 +16,54 @@ class _HomePageState extends State<HomePage> {
   bool _isUploading = false;
   final SupabaseClient supabase = Supabase.instance.client;
 
+  Future<void> _pickAndUploadToPublicBucket() async {
+    final picker = ImagePicker();
+    final picked = await picker.pickImage(source: ImageSource.gallery);
+
+    if (picked == null) return;
+
+    setState(() => _isUploading = true);
+
+    try {
+      final fileName =
+          '${DateTime.now().millisecondsSinceEpoch}_${picked.name}';
+      final filePath = 'uploads/$fileName';
+
+      if (kIsWeb) {
+        final bytes = await picked.readAsBytes();
+        await supabase.storage
+            .from('bucket_images')
+            .uploadBinary(
+              filePath,
+              bytes,
+              fileOptions: const FileOptions(contentType: 'image/*'),
+            );
+      } else {
+        final file = File(picked.path);
+        await supabase.storage.from('bucket_images').upload(filePath, file);
+      }
+
+      final publicUrl = supabase.storage
+          .from('bucket_images')
+          .getPublicUrl(filePath);
+
+      setState(() {
+        _publicImageUrl = publicUrl;
+      });
+    } catch (e) {
+      debugPrint('Error Upload: $e');
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Failed Upload: $e')));
+      }
+    } finally {
+      if (mounted) {
+        setState(() => _isUploading = false);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
